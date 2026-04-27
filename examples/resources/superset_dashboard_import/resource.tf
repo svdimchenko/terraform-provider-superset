ec0b6305d87cb0c7eef79daf31322ffbb717e86f
Serhii Dimchenko <svdimchenko@gmail.com>
2026-04-27 14:54:52 +0200

    Add database override

--- examples/resources/superset_dashboard_import/resource.tf
+++ examples/resources/superset_dashboard_import/resource.tf
@@ -9,8 +9,8 @@ resource "superset_dashboard_import" "example" {
   # Override any database YAML fields (including nested) by UUID.
   # Value is a JSON-encoded object that gets deep-merged into the YAML.
   database_overrides = {
-    "dd568dff-7835-4cee-8e42-c91f3b533c49" = jsonencode({
-      sqlalchemy_uri = "awsathena+rest://athena.eu-central-1.amazonaws.com/?s3_staging_dir=s3%3A%2F%2Fstage-dwh-athena%2Fresults%2Fsuperset&work_group=superset"
+    "dd568dff-1234-4cee-8e42-c91f3b533c49" = jsonencode({
+      sqlalchemy_uri = "awsathena+rest://athena.eu-central-1.amazonaws.com/?s3_staging_dir=s3%3A%2F%2Ftest-athena%2Fresults%2Fsuperset&work_group=test"
       extra = {
         schemas_allowed_for_file_upload = ["dev_sandbox"]
         cost_estimate_enabled           = false
