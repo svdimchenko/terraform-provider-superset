1c5419f202a39be6b4cfa58d96c416e13b32b77e
Serhii Dimchenko <svdimchenko@gmail.com>
2026-05-19 17:58:12 +0200

    Fix

--- examples/resources/superset_dashboard_embedding/resource.tf
+++ examples/resources/superset_dashboard_embedding/resource.tf
@@ -1,5 +1,5 @@
 resource "superset_dashboard_embedding" "example" {
-  dashboard_id   = superset_dashboard_import.example.dashboard_id
+  dashboard_id    = superset_dashboard_import.example.dashboard_id
   allowed_domains = ["https://app.example.com", "https://portal.example.com"]
 }
 
