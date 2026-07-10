c8109b2290e753684a4f88b7ab278e46a83d4800
Roman Korsun <romakorsun2000@gmail.com>
2026-07-10 14:03:09 +0300

    replace uuid with anonymys placeholders for dashboards import too

--- examples/resources/superset_dashboard_import/resource.tf
+++ examples/resources/superset_dashboard_import/resource.tf
@@ -1,18 +1,18 @@
 resource "superset_dashboard_import" "example" {
-  source_dir      = "${path.module}/dashboards/athena_usage"
+  source_dir      = "${path.module}/dashboards/my_dashboard"
   force_overwrite = true
 
   database_secrets = {
-    "dd568dff-7835-4cee-8e42-c91f3b533c49" = var.athena_db_password
+    "db-uuid" = var.db_password
   }
 
   # Override any database YAML fields (including nested) by UUID.
   # Value is a JSON-encoded object that gets deep-merged into the YAML.
   database_overrides = {
-    "dd568dff-1234-4cee-8e42-c91f3b533c49" = jsonencode({
-      sqlalchemy_uri = "awsathena+rest://athena.eu-central-1.amazonaws.com/?s3_staging_dir=s3%3A%2F%2Ftest-athena%2Fresults%2Fsuperset&work_group=test"
+    "db-uuid" = jsonencode({
+      sqlalchemy_uri = "postgresql://user:pass@host:5432/mydb"
       extra = {
-        schemas_allowed_for_file_upload = ["dev_sandbox"]
+        schemas_allowed_for_file_upload = ["public"]
         cost_estimate_enabled           = false
       }
     })
